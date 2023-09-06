a4edf7cf2cdf14a60bb9927a6cfd3bd1870f89db
Ida Brandt <92617233+ponchikidonchik@users.noreply.github.com>
2023-09-06 22:59:39 +0500

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿
+//░█ 
+
+string a = ".";
+string b = "#";
+Console.WriteLine($"{a}{a}{a}{a}{a}{a}{a}{a}{a}{a}{a}{a}{a}{a}{a}");
+Console.WriteLine($"{a}{a}{a}{a}{b}{a}{a}{b}{a}{a}{b}{a}{a}{a}{a}");
+Console.WriteLine($"{a}{a}{a}{b}{a}{a}{a}{b}{a}{b}{a}{a}{a}{a}{a}");
+Console.WriteLine($"{a}{a}{a}{b}{a}{a}{b}{a}{a}{b}{a}{a}{a}{a}{a}");
+Console.WriteLine($"{a}{a}{a}{a}{b}{a}{b}{a}{a}{a}{b}{a}{a}{a}{a}");
+Console.WriteLine($"{a}{a}{a}{a}{b}{a}{a}{b}{a}{a}{b}{a}{a}{a}{a}");
+Console.WriteLine($"{a}{a}{a}{a}{a}{a}{a}{a}{a}{a}{a}{a}{a}{a}{a}");
+Console.WriteLine($"{a}{a}{b}{b}{b}{b}{b}{b}{b}{b}{b}{b}{a}{a}{a}");
+Console.WriteLine($"{a}{a}{b}{a}{a}{a}{a}{a}{a}{a}{a}{b}{b}{b}{a}");
+Console.WriteLine($"{a}{a}{b}{a}{a}{a}{a}{a}{a}{a}{a}{b}{a}{b}{a}");
+Console.WriteLine($"{a}{a}{b}{a}{a}{a}{a}{a}{a}{a}{a}{b}{a}{b}{a}");
+Console.WriteLine($"{a}{a}{a}{b}{a}{a}{a}{a}{a}{a}{b}{a}{a}{a}{a}");
+Console.WriteLine($"{a}{b}{b}{b}{b}{b}{b}{b}{b}{b}{b}{b}{b}{b}{a}");
+Console.WriteLine($"{a}{b}{a}{a}{a}{a}{a}{a}{a}{a}{a}{a}{b}{a}{a}");
+Console.WriteLine($"{a}{a}{b}{b}{b}{b}{b}{b}{b}{b}{b}{b}{a}{a}{a}");
+Console.WriteLine($"{a}{a}{a}{a}{a}{a}{a}{a}{a}{a}{a}{a}{a}{a}{a}");
+Console.ReadLine();
